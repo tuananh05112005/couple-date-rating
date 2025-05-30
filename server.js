18600eb9d015697e17bbaac0edf1ee9c6a498527
tuananh05112005 <huynhnguyentuananh0511@gmail.com>
2025-05-31 03:38:35 +0700

    all

--- server.js
+++ server.js
@@ -9,11 +9,12 @@ app.use(bodyParser.json());
 app.use(express.static('src')); // phục vụ index.html và tệp tĩnh
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',        // đổi nếu bạn đặt khác
-  password: '05112005',        // nhập mật khẩu MySQL của bạn
-  database: 'couple_rating'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
+
 
 db.connect((err) => {
   if (err) throw err;
